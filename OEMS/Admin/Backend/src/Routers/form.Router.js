4f0b50fe300f69c287b527a8230c21b5f5508a9d
web-ghost-thiru <thirumurugankutty@gmail.com>
2025-04-30 13:38:42 +0530

    Added getFormByIdQuery

--- OEMS/Admin/Backend/src/Routers/form.Router.js
+++ OEMS/Admin/Backend/src/Routers/form.Router.js
@@ -8,6 +8,7 @@ router.use(authenticateJWT,authenticateSession);
  
 router.get("/form/:formId/fields", formController.getFields);
 router.get('/forms', formController.getForms);
+router.get("/form/:formId", formController.getFormById);
 router.post("/form/refresh-token", formController.refreshToken);
 router.put("/form/:formId/field/:fieldId", formController.updateField);
 router.delete("/form/:formId/field/:fieldId", formController.deleteField);
